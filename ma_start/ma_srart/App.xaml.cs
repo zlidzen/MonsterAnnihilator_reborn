f3a934d4efc8afbef9e4586ab80ba6eda3007881
zlidzen <m.karsakou@gmail.com>
2017-10-19 23:53:03 +0400

    - shit!!!

---
+++ ma_start/ma_srart/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace ma_start
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
